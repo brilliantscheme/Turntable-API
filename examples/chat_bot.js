846772819a75bf67fe7b73ef9f86686786f6216f
brilliantscheme <princeofcarlsbad@gmail.com>
2012-12-26 17:16:40 -0800

    Update examples/chat_bot.js

--- examples/chat_bot.js
+++ examples/chat_bot.js
@@ -1,7 +1,7 @@
 var Bot    = require('../index');
-var AUTH   = 'auth+live+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-var USERID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
-var ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+var AUTH   = 'auth+live+d0bcd6b9fe29b11b00dae66ad0a44635a937f938';
+var USERID = '50047146aaa5cd359e00004c';
+var ROOMID = '50d8d034eb35c14a2c232aa6';
 
 var bot = new Bot(AUTH, USERID, ROOMID);
 
